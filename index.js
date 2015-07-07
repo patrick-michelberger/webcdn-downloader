ed6e1041b56fdb95d3b405760573218fb72324b9
patrick_michelberger <pmichelberger@gmx.de>
2015-07-07 14:22:42 +0200

    cheerio added & multiple website loader added

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
+// Dependencies
+var request = require('request');
+var cheerio = require('cheerio');
 var Downloader = require('./lib/downloader.js');
+var fs = require('fs');
 
-Downloader.load('https://secure-i1.ztat.net//camp/68/ae/b05db993d41d33bb22401f68a0c0.jpg', function(err) {
-	if (err) {
-		console.log("Error: ", err);
-	} else {
-	    console.log('S3 upload finished!');
-	}
-});
+var downloader = new Downloader("webcdn2");
+
+var x = 0;
+var loadWebsites = function(websites) {
+    var website = websites[x];
+    request(website.url, function(error, response, html) {
+        if (!error && response.statusCode == 200) {
+            var $ = cheerio.load(html);
+            var i = 0;
+            var loopImages = function(arr) {
+                var url = arr[i].attribs['data-webcdn-fallback'];
+                downloader.load(url, function(err) {
+                    i++;
+                    if (err) {
+                        console.log("Error: ", err);
+                    } else {
+                        console.log('S3 upload finished for: ', url);
+                    }
+                    if (i < arr.length) {
+                        loopImages(arr);
+                    } else {
+                        x++;
+                        if (x < websites.length) {
+                            loadWebsites(websites);
+                        }
+                    }
+                });
+            };
+            loopImages($('img'));
+        } else {
+            x++;
+            if (x < websites.length) {
+                loadWebsites(websites);
+            }
+        }
+    });
+};
+
+// Main
+var websites = JSON.parse(fs.readFileSync('./websites.json', 'utf8'));
+loadWebsites(websites);
